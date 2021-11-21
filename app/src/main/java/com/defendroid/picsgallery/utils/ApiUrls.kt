2d9dc5df31cb3eb9f800e63c52aff01b3043eee6
Mamun <hasan.zaman.mamun@gmail.com>
2021-11-21 12:22:11 +0600

    * ApiUrls.kt and AppConstants.kt added

---
+++ app/src/main/java/com/defendroid/picsgallery/utils/ApiUrls.kt
@@ -0,0 +1,9 @@
+package com.defendroid.picsgallery.utils
+
+object ApiUrls {
+
+    private const val BASE_URL = "https://picsum.photos/"
+    private const val VERSION = "v2"
+
+    const val PHOTO_LIST_URL = "$BASE_URL$VERSION/list"
+}
---
+++ app/src/main/java/com/defendroid/picsgallery/utils/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.defendroid.picsgallery.utils
+
+object AppConstants {
+    const val THUMB_HEIGHT = "200"
+    const val THUMB_WIDTH = "300"
+}
